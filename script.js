880f21bb0106b08f6acfa20a7d0e5b8e901b4e5f
Flagguessr <cas210320@spengergasse.at>
2023-09-12 10:31:46 +0200

    Test

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const ul = document.getElementById("meineUL");
+
+
+function addElement()
+{
+    const li = document.createElement("li");
+    const text2 = document.getElementById("text").value;
+    li.innerHTML = text2;
+    ul.appendChild(li);
+}
