8b6ef3edc6483860cca1d20932c75609e44b018a
Flagguessr <cas210320@spengergasse.at>
2023-10-10 09:02:43 +0200

    u89

--- 10/10/9/23 promise/function.js
+++ 10/10/9/23 promise/function.js
@@ -2,7 +2,7 @@
     function f() {
         const u =0;
         while(u <10){
-            let zufallszahl = Math.round(Math.random() *28 +1);
+            let zufallszahl = Math.round(Math.random() *10 +1);
             if(zufallszahl > 30){
                 throw new Error ("Fehler!");
                 return fertigstellung;
