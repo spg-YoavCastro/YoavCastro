5c30b3e4383a53b55b3aea1d7976053f458262a3
Flagguessr <cas210320@spengergasse.at>
2023-12-02 16:58:51 +0100

    commit

---
+++ 2023-11-28/script.js
@@ -0,0 +1,22 @@
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const input3 = document.getElementById("input3");
+
+const output = document.getElementById("output");
+output.publish = function (arg) {
+    this.innerHTML = JSON.stringify(arg);
+};
+
+const button_push = document.getElementById("button_push");
+button_push.addEventListener("click", push);
+
+const button_indexof = document.getElementById("button_indexof");
+const button_includes = document.getElementById("button_includes");
+const button_slice = document.getElementById("button_slice");
+
+function push() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    i1.push(i2);
+    output.publish(i1);
+}
