3aab3f6f2498f68b09b71dc0a5b9035c0bf93ba6
Flagguessr <cas210320@spengergasse.at>
2024-01-09 11:07:00 +0100

    commit

---
+++ app.js
@@ -0,0 +1,31 @@
+class Person{
+    /* Gewicht in kg, Größe in m*/
+    name;
+
+    #gewicht;
+
+    groesse;
+
+    constructor(name, gewicht, groesse){
+        this.name = name;
+        this.gewicht = gewicht;
+        this.groesse = groesse;
+
+    }
+    get bmi(){
+        return this.#gewicht/(this.groesse*this.groesse);
+    }
+    
+    set gewicht(gewichtPar){
+        //gewicht in kg
+        if(gewichtPar < 1 || gewichtPar > 500){
+            throw new Error("ungültiges Gewicht")
+        }
+        this.#gewicht = gewichtPar;
+    }
+    get gewicht(){
+        return this.#gewicht;
+    }
+}
+p = new Person('Hans', 65, 1.75);
+console.log(p.bmi);
