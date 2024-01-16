b5aabc013282f37503addece924df112aae91c3d
YoavCastro <cas210320@spengergasse.at>
2024-01-16 10:06:14 +0100

    commit

--- 2024-1-9 Klassen/app.js
+++ 2024-1-9 Klassen/app.js
@@ -25,7 +25,7 @@ class Person{
         this.#gewicht = gewichtPar;
     }
     set groesse(groessePar){
-        //gewicht in kg
+        //groesse in m
         if(groessePar < 0,5 || groessePar > 3){
             throw new Error("ungültige Größe")
         }
@@ -42,7 +42,7 @@ p1 = new Person('Hans', 65, 1.75);
 p2 = new Person('Michi', 80, 1.70);
 p3 = new Person('Sepp', 120, 2.1);
 p4 = new Person('Felix', 40, 1.2);
-console.log("Person 1:"p1.bmi);
+console.log(p1.bmi);
 console.log(p2.bmi);
 console.log(p3.bmi);
 console.log(p4.bmi);
