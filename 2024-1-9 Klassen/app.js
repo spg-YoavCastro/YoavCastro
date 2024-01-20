3e920ef9054887c639c0adb3794aefccc6655475
Flagguessr <cas210320@spengergasse.at>
2024-01-20 18:29:24 +0100

    commit

--- 2024-1-9 Klassen/app.js
+++ 2024-1-9 Klassen/app.js
@@ -1,46 +1,79 @@
-class Person{
-    /* Gewicht in kg, Größe in m*/
-    name;
+// So liegt der Normalwert bei Männern laut Deutscher Gesellschaft für Ernährung
+// im Intervall von 20 bis 25 kg / m², während er sich
+// bei Frauen im Intervall von 19 bis 24 kg / m² befindet.
 
+class Person {
+    /* Gewicht in kg, Größe in m */
+    #name;
     #gewicht;
-
     #groesse;
-
-    constructor(name, gewicht, groesse){
-        this.name = name;
-        this.gewicht = gewicht;
-        this.groesse = groesse;
-
+    #geschlecht;
+    constructor(namePar, gewichtPar, groessePar, GeschlechtPar) {
+        this.name = namePar;
+        this.gewicht = gewichtPar;
+        this.groesse = groessePar;
+        this.#geschlecht = geschlechtPar;
     }
-    get bmi(){
-        return Math.round(this.#gewicht/(this.#groesse*this.#groesse)).toFixed(1);
 
+
+    set name(namePar) {
+        if (typeof namePar !== 'string') {
+            throw new Error('ungültiger Name');
+        }
+        if (namePar.length < 3) {
+            throw new Error('Name zu kurz');
+        }
+        this.#name = namePar;
+        document.getElementById(namePar);
     }
-    
-    set gewicht(gewichtPar){
-        //gewicht in kg
-        if(gewichtPar < 1 || gewichtPar > 500){
-            throw new Error("ungültiges Gewicht")
+
+    set gewicht(gewichtPar) {
+        // gewicht in kg
+        if (gewichtPar < 10 || gewichtPar > 500) {
+            throw new Error('ungültiges Gewicht');
         }
         this.#gewicht = gewichtPar;
+        document.getElementById(gewichtPar);
     }
-    set groesse(groessePar){
-        //groesse in m
-        if(groessePar < 0,5 || groessePar > 3){
-            throw new Error("ungültige Größe")
+    get gewicht() {
+        return this.#gewicht;
+    }
+    set groesse(groessePar) {
+        if (groessePar < 0.5 || groessePar > 3.0) {
+            throw new Error('ungültige Groesse');
         }
         this.#groesse = groessePar;
-    }
-    get gewicht(){
-        return this.#gewicht;
+        document.getElementById(groessePar);
     }
     get groesse(){
         return this.#groesse;
     }
+    set geschlecht(geschlechtPar) {
+        if (geschlechtPar !== 'Männlich' && geschlechtPar !== 'Weiblich') {
+            throw new Error('Ungültiges Geschlecht');
+        }
+        this.#geschlecht = geschlechtPar;
+    }
+
+    get geschlecht() {
+        return this.#geschlecht;
+    }
+
+    get bmi() {
+        const nmbr = this.#gewicht / (this.#groesse * this.#groesse);
+        return Math.round(nmbr * 10) / 10;
+    }
+    
+    toString() {
+        return (
+            'Name: ' +
+            this.#name +
+            ' Gewicht: ' +
+            this.#gewicht +
+            ' Größe: ' +
+            this.#groesse +
+            ' BMI: ' +
+            this.bmi
+        );
+    }
 }
-p1 = new Person('Hans', 65, 1.75), console.log( p1.bmi);
-p2 = new Person('Michi', 80, 1.70), console.log(p2.bmi);
-p3 = new Person('Sepp', 120, 2.1), console.log(p3.bmi);
-p4 = new Person('Felix', 40, 1.2), console.log(p4.bmi);
-
-
