d1fb38107fc4acf5b305eb553b5662fb077562ed
spg-YoavCastro <cas210320@spengergasse.at>
2024-05-27 21:11:38 +0200

    seed.js abteilungen

--- PLF5/Teil1/seed.js
+++ PLF5/Teil1/seed.js
@@ -5,17 +5,32 @@ async function main() {
     // throw new Error('XY');
     let i = 0;
     let zooCount = 5;
+    const minAbteilungen = 2;
+    const maxAbteilungen = 7;
+    const minTiere = 5;
+    const maxTiere = 20;
 
     for (i = 0; i < zooCount; i++) {
         const zoo = {
             land: fakerDE.location.country(),
             stadt: fakerDE.location.city(),
             adresse: fakerDE.location.streetAddress(),
-            baujahr: fakerDE.date.past().getFullYear()
+            baujahr: fakerDE.date.past().getFullYear(),
+            abteilungen: { 
+                create: Array.from({length:fakerDE.datatype.number({min:minAbteilungen, max:maxAbteilungen})}).map(() => ({
+                    name: fakerDE.animal.type(), 
+                    tiere:{
+                    create: Array.from({length:fakerDE.datatype.number({min:minTiere, max:maxTiere})}).map(() => ({name: fakerDE.name.firstName(), art: fakerDE.animal.type(),
+    
+                    })),
+                },
+            })),
+            }
         };
         await prisma.zoo.create({ data: zoo });
     }
     return 'Alles ist gut';
+
 }
 main()
     .then((rw) => console.log('seeding done: ', rw))
